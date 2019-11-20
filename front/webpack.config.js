ace3b915e79f08e81363c62fe6ed16bab0a2a20a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-20 17:38:00 +0100

    add mode webpack

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -2,20 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: ['./src/main.ts'],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[hash].js',
-  },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' }), new CleanWebpackPlugin()],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
+module.exports = (env, argv) => {
+  console.log('mode', argv.mode); // outputs development
+  return {
+    entry: ['./src/main.ts'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.[hash].js',
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' }), new CleanWebpackPlugin()],
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
 };
