cdb510c2c53badf51aa7bbf55d24b55a8d93a5fd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-20 18:17:21 +0100

    add sass/scss

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = (env, argv) => {
   console.log('mode', argv.mode); // outputs development
   return {
-    entry: ['./src/main.ts'],
+    entry: ['./src/main.ts', './src/styles.scss'],
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.[hash].js',
@@ -28,6 +28,17 @@ module.exports = (env, argv) => {
           test: /\.ts$/,
           use: 'ts-loader',
           exclude: /node_modules/,
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
         },
       ],
     },
