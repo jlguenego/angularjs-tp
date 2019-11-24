70bf4a742ea16e7f672e7a149ecd4f7bfc7bb933
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-24 18:59:06 +0100

    activate lazyloading

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,25 +1,29 @@
 import '@uirouter/angularjs';
+import { StateProvider } from '@uirouter/angularjs';
 import { module } from 'angular';
-import './add-reference/add-reference.module';
+import 'oclazyload';
 import './layout/layout.module';
 
-module('app', ['layout', 'ui.router', 'app.addReference'])
+module('app', ['layout', 'ui.router', 'oc.lazyLoad'])
   .component('appRoot', require('./app.component'))
   .component('appHome', require('./routes/home/home.component'))
   .component('appLegal', require('./routes/legal/legal.component'))
-  .config(['$stateProvider', ($stateProvider: any) => {
-    const homeState = {
-      name: 'home',
-      url: '/',
-      component: 'appHome',
-    };
-
-    const legalState = {
-      name: 'legal',
-      url: '/legal',
-      component: 'appLegal',
-    };
-
-    $stateProvider.state(homeState);
-    $stateProvider.state(legalState);
-  }]);
+  .config([
+    '$stateProvider',
+    ($stateProvider: StateProvider) => {
+      $stateProvider.state({ name: 'home', url: '/', component: 'appHome' });
+      $stateProvider.state({ name: 'legal', url: '/legal', component: 'appLegal' });
+      $stateProvider.state({
+        name: 'add-reference-form.**',
+        url: '/add-reference-form',
+        lazyLoad: async (transition, state) => {
+          await import('./add-reference/add-reference.module');
+          transition
+            .injector()
+            .get('$ocLazyLoad')
+            .load({ name: 'app.addReference' });
+          return {};
+        },
+      });
+    },
+  ]);
