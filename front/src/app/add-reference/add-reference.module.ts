6b42e4b8bf479f86f91a44617d7a341c30a24074
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-24 18:58:37 +0100

    improve code of add-reference module

--- front/src/app/add-reference/add-reference.module.ts
+++ front/src/app/add-reference/add-reference.module.ts
@@ -1,23 +1,14 @@
+import { StateProvider } from '@uirouter/angularjs';
 import { module } from 'angular';
+
+console.log('about to declare app.addReference');
 
 module('app.addReference', ['ui.router'])
   .component('appAddReferenceForm', require('./form/form.component'))
   .component('appAddReferenceSuccess', require('./success/success.component'))
   .config([
     '$stateProvider',
-    ($stateProvider: any) => {
-      const homeState = {
-        name: 'home',
-        url: '/',
-        component: 'appHome',
-      };
-
-      const legalState = {
-        name: 'legal',
-        url: '/legal',
-        component: 'appLegal',
-      };
-
+    ($stateProvider: StateProvider) => {
       $stateProvider.state({
         name: 'add-reference-form',
         url: '/add-reference/form',
