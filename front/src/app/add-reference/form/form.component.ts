d6ad933d9be164a6db68b1bdd49a5ec98c49ffba
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-30 11:01:41 +0100

    add form

--- front/src/app/add-reference/form/form.component.ts
+++ front/src/app/add-reference/form/form.component.ts
@@ -1,5 +1,27 @@
+import { IScope } from 'angular';
+
 require('./form.component.scss');
+
+interface AddReferenceFormData {
+  label?: string;
+  category?: string;
+  quantity?: number;
+  price?: number;
+}
 
 module.exports = {
   template: require('./form.component.html'),
+  controller: class FormController {
+    data: AddReferenceFormData = {};
+    constructor($scope: IScope) {
+      'ngInject';
+      $scope.$watch(
+        '$ctrl.data',
+        () => {
+          console.log('$ctrl.data', this.data);
+        },
+        true
+      );
+    }
+  },
 };
