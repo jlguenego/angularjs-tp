d562be68ece18471d837d076c6fd855af020c8c3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-30 15:11:29 +0100

    cors

--- back/server.ts
+++ back/server.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import * as serveIndex from "serve-index";
+import * as cors from "cors";
 
 const www = ".";
 const port = 3000;
 
 const app = express();
+app.use(cors());
 
 app.use(express.static(www));
 app.use(serveIndex(www, { icons: true }));
