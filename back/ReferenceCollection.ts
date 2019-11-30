e723a29617349ed6c20c82d76a16961181b429bb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-30 16:33:23 +0100

    typings

--- back/ReferenceCollection.ts
+++ back/ReferenceCollection.ts
@@ -1,13 +1,15 @@
+import { Reference } from "../front/src/app/interfaces/Reference";
+
 export class ReferenceCollection {
-  refs = [];
-  push(ref: any) {
-    const data = { ...ref, id: this.newId() };
+  refs: Reference[] = [];
+  push(ref: Reference): Reference {
+    const data: Reference = { ...ref, id: this.newId() };
     this.refs.push(data);
     return data;
   }
 
-  newId(): number {
-    const id = Math.max(0, ...this.refs.map(d => d.id));
-    return id + 1;
+  newId(): string {
+    const id = Math.max(0, ...this.refs.map(d => +d.id));
+    return `${id + 1}`;
   }
 }
