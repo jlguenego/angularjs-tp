cf06c4c09dd2c857058ac1cf74a240a692901b91
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-30 18:09:39 +0100

    list component

--- front/src/app/sell-reference/list/list.component.ts
+++ front/src/app/sell-reference/list/list.component.ts
@@ -1,5 +1,12 @@
+import { ReferenceService } from '../../services/reference.service';
+
 require('./list.component.scss');
 
 module.exports = {
   template: require('./list.component.html'),
+  controller: class ListController {
+    constructor(public reference: ReferenceService) {
+      'ngInject';
+    }
+  },
 };
