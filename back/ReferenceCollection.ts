93b94d3c27568922aac95ad118c7c7bb0dc4806e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-30 16:52:11 +0100

    add file persistence

--- back/ReferenceCollection.ts
+++ back/ReferenceCollection.ts
@@ -1,15 +1,29 @@
 import { Reference } from "../front/src/app/interfaces/Reference";
+import * as fs from "fs";
+import * as path from "path";
+
+const filename = path.resolve(__dirname, 'data.json');
 
 export class ReferenceCollection {
-  refs: Reference[] = [];
+  refs = this.getRefs();
   push(ref: Reference): Reference {
     const data: Reference = { ...ref, id: this.newId() };
     this.refs.push(data);
+    fs.writeFileSync(filename, JSON.stringify(this.refs));
     return data;
   }
 
   newId(): string {
     const id = Math.max(0, ...this.refs.map(d => +d.id));
     return `${id + 1}`;
+  }
+
+  getRefs(): Reference[] {
+    try {
+      const str = fs.readFileSync(filename, {encoding: 'utf8'});
+      return JSON.parse(str);
+    } catch (e) {
+      return [];
+    }
   }
 }
