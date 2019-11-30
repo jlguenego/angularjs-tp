6120505a4aa0c2028c61c0e031a51f73e6e80e02
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-30 12:43:51 +0100

    implement service with localstorage

--- front/src/app/services/reference.service.ts
+++ front/src/app/services/reference.service.ts
@@ -1,7 +1,23 @@
 import { Reference } from '../interfaces/Reference';
 
 export class ReferenceService {
+  references = this.getReferences();
+
   add(ref: Reference) {
     console.log('adding ref: ', ref);
+    this.references.push(ref);
+    this.setReferences();
+  }
+
+  setReferences() {
+    localStorage.setItem('references', JSON.stringify(this.references));
+  }
+
+  getReferences(): Reference[] {
+    const str = localStorage.getItem('references');
+    if (!str) {
+      return [];
+    }
+    return JSON.parse(str);
   }
 }
